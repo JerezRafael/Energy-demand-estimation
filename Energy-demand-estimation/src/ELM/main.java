a51f94cd862712e671553b6ed557e15e58f9560e
Rafael Jerez <rafa.jerez4@gmail.com>
2018-03-01 17:00:48 +0100

    Puerta AND creada en memoria

--- Energy-demand-estimation/src/ELM/main.java
+++ Energy-demand-estimation/src/ELM/main.java
@@ -1,5 +1,7 @@
 package ELM;
 
+import java.util.Arrays;
+
 import no.uib.cipr.matrix.NotConvergedException;
 
 public class main {
@@ -9,30 +11,15 @@ public class main {
 	 * @throws NotConvergedException
 	 */
 	public static void main(String[] args) throws NotConvergedException {
-		// TODO Auto-generated method stub
-		elm ds1 = new elm(0, 20, "sig");
-		ds1.train("src\\ELM\\sinc_train");
-		ds1.test("src\\ELM\\sinc_test");
-
-		elm ds2 = new elm(1, 20, "sig");
-		ds2.train("src\\ELM\\diabetes_train");
-		ds2.test("src\\ELM\\diabetes_test");
-
-		elm ds3 = new elm(1, 20, "sig");
-		ds3.train("src\\ELM\\AND_train");
-		ds3.test("src\\ELM\\AND_test");
-
-		System.out.println("TrainingTime:" + ds1.getTrainingTime());
-		System.out.println("TrainingAcc:" + ds1.getTrainingAccuracy());
-		System.out.println("TestingTime:" + ds1.getTestingTime());
-		System.out.println("TestAcc:" + ds1.getTestingAccuracy());
-		System.out.println();
-
-		System.out.println("TrainingTime:" + ds2.getTrainingTime());
-		System.out.println("TrainingAcc:" + ds2.getTrainingAccuracy());
-		System.out.println("TestingTime:" + ds2.getTestingTime());
-		System.out.println("TestAcc:" + ds2.getTestingAccuracy());
-		System.out.println();
+		
+		// puerta AND
+		System.out.println("\nTest AND:");
+		elm ds3 = new elm(0, 20, "sig");
+		double[][] traindata = new double[][] { { 1, 0, 0 }, { 0, 0, 1 }, { 0, 1, 0 }, { 1, 1, 1 } };
+		ds3.train(traindata);
+		double[][] inpt = new double[][] { { 5000, 0, 0 }, { 5000, 0, 1 }, { 5000, 1, 0 }, { 5001, 1, 1 } };
+		double[] salida = ds3.testOut(inpt);
+		System.out.println(Arrays.toString(salida));
 
 		System.out.println("TrainingTime:" + ds3.getTrainingTime());
 		System.out.println("TrainingAcc:" + ds3.getTrainingAccuracy());
