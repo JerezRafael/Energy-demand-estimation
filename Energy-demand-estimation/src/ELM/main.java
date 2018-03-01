a225767f5c5dca9048a89ba5d4bffb5102b44576
Rafael Jerez <rafa.jerez4@gmail.com>
2018-03-01 16:58:01 +0100

    Pruebas con puerta AND

--- Energy-demand-estimation/src/ELM/main.java
+++ Energy-demand-estimation/src/ELM/main.java
@@ -18,6 +18,10 @@ public class main {
 		ds2.train("src\\ELM\\diabetes_train");
 		ds2.test("src\\ELM\\diabetes_test");
 
+		elm ds3 = new elm(1, 20, "sig");
+		ds3.train("src\\ELM\\AND_train");
+		ds3.test("src\\ELM\\AND_test");
+
 		System.out.println("TrainingTime:" + ds1.getTrainingTime());
 		System.out.println("TrainingAcc:" + ds1.getTrainingAccuracy());
 		System.out.println("TestingTime:" + ds1.getTestingTime());
@@ -28,6 +32,13 @@ public class main {
 		System.out.println("TrainingAcc:" + ds2.getTrainingAccuracy());
 		System.out.println("TestingTime:" + ds2.getTestingTime());
 		System.out.println("TestAcc:" + ds2.getTestingAccuracy());
+		System.out.println();
+
+		System.out.println("TrainingTime:" + ds3.getTrainingTime());
+		System.out.println("TrainingAcc:" + ds3.getTrainingAccuracy());
+		System.out.println("TestingTime:" + ds3.getTestingTime());
+		System.out.println("TestAcc:" + ds3.getTestingAccuracy());
+		System.out.println();
 
 	}
 
