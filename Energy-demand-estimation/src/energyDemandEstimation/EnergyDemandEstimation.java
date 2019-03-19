51613bd20d1bd42efc277acf0c1e76e2fc4d2c8c
JerezRafael <rafa.jerez4@gmail.com>
2019-03-19 11:23:54 +0100

    Automatización de pruebas
    - Ahora las excepciones se capturan y se intenta de nuevo la busqueda
    local para automatizar más las pruebas.
    - Extracción de resultados antes de la busqueda local para ver la
    factibilidad de la solución candidata creada por los constructores.
    - Se realizan 10 ejecuciones y se guardan en un .csv indicando el año

--- Energy-demand-estimation/src/energyDemandEstimation/EnergyDemandEstimation.java
+++ Energy-demand-estimation/src/energyDemandEstimation/EnergyDemandEstimation.java
@@ -4,6 +4,9 @@ import energyDemandEstimation.data.*;
 import energyDemandEstimation.misc.RandomManager;
 import energyDemandEstimation.misc.Solution;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 import energyDemandEstimation.ELM.elm;
@@ -17,7 +20,7 @@ public class EnergyDemandEstimation {
 
 	final static int nIterations = 100;
 	final static int añoInicio = 1981;
-	final static int añoBuscado = 2011;
+	final static int añoBuscado = 1999;
 	final static int añosTrain = añoBuscado - añoInicio;
 
 	public static void main(String[] args) throws NotConvergedException {
@@ -34,146 +37,266 @@ public class EnergyDemandEstimation {
 		int year, numVars = 0;
 		double[] output;
 		double[][] testData, trainData = null;
-
-		System.out.println("-----Random Constructive-----");
-
-		constructive = new CRandom();
-
-		bestSol = null;
-		bestAccuracy = Double.MAX_VALUE;
-
-		for (int i = 0; i < nIterations; i++) {
-			sol = constructive.generateSolution();
-			numVars = 0;
-
-			// Contamos el número de variables escogidas
-			for (int j = 0; j < sol.getSelectedVars().length; j++) {
-				if (sol.getSelectedVars()[j])
-					numVars++;
-			}
-
-			// Cogemos los años anteriores al que vamos a testear
-			trainData = new double[4][numVars];
-			year = añoInicio;
-			for (int j = 0; j < trainData.length; j++) {
-				trainData[j] = data.getYear(year, sol.getSelectedVars());
-				year++;
-			}
-
-			// Se prueba la posible solución
-			elm = new elm(0, 20, "sig");
-			elm.train(trainData);
-
-			if (elm.getTrainingAccuracy() < bestAccuracy) { // Si es mejor se guarda
-				bestAccuracy = elm.getTrainingAccuracy();
-				bestSol = sol;
-				for (int j = 0; j < mostUsedVars.length; j++) {
-					if (sol.getSelectedVars()[j])
-						mostUsedVars[j]++;
-				}
-			}
-		}
-
-		bestSol = localSearch.improve(bestSol, mostUsedVars);
-
-		numVars = 0;
-		for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
-			if (bestSol.getSelectedVars()[i])
-				numVars++;
-		}
-		trainData = new double[añosTrain][numVars];
-		year = añoInicio;
-		for (int i = 0; i < trainData.length; i++) {
-			trainData[i] = data.getYear(year, bestSol.getSelectedVars());
-			year++;
-		}
-
-		testData = new double[2][];
-		testData[0] = data.getYear(year, bestSol.getSelectedVars());
-		testData[1] = data.getYear(year, bestSol.getSelectedVars());
+		boolean exception;
 		System.out.println("Año buscado: " + añoBuscado);
 
-		elm = new elm(0, 20, "sig");
-		elm.train(trainData);
-		numVars = 0;
-		for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
-			if (bestSol.getSelectedVars()[i])
-				numVars++;
-		}
-		output = elm.testOut(testData);
+		/* CSV */
+		PrintWriter pw;
+		try {
+			pw = new PrintWriter(new File(añoBuscado + ".csv"));
+			StringBuilder sb = new StringBuilder();
+			sb.append(añoBuscado);
+			sb.append(";");
+			sb.append("CRandom");
+			sb.append(";");
+			sb.append("Total CR");
+			sb.append(";");
+			sb.append("CVotos");
+			sb.append(";");
+			sb.append("Total CV");
+			sb.append("\n");
 
-		System.out.println(Arrays.toString(output));
+			// Bucle para obtener 10 resultados y exportarlos a un csv
+			for (int n = 0; n < 10; n++) {
+				
+				sb.append(n + 1);
+				sb.append(";");
+				System.out.println("\nn = " + (n + 1));
+				System.out.println("\n-----Random Constructive-----");
+				
+				constructive = new CRandom();
 
-////////////////////////////////////////////////////////////////////////////
+				bestSol = null;
+				bestAccuracy = Double.MAX_VALUE;
 
-		System.out.println("\n-----Votos Constructive-----");
+				for (int i = 0; i < nIterations; i++) {
+					sol = constructive.generateSolution();
+					numVars = 0;
 
-		constructive = new CVotos(nIterations);
+					// Contamos el número de variables escogidas
+					for (int j = 0; j < sol.getSelectedVars().length; j++) {
+						if (sol.getSelectedVars()[j])
+							numVars++;
+					}
 
-		bestSol = null;
-		bestAccuracy = Double.MAX_VALUE;
+					// Cogemos los años anteriores al que vamos a testear
+					trainData = new double[4][numVars];
+					year = añoInicio;
+					for (int j = 0; j < trainData.length; j++) {
+						trainData[j] = data.getYear(year, sol.getSelectedVars());
+						year++;
+					}
 
-		for (int i = 0; i < nIterations; i++) {
-			sol = constructive.generateSolution();
-			numVars = 0;
+					// Se prueba la posible solución
+					elm = new elm(0, 20, "sig");
+					elm.train(trainData);
 
-			// Contamos el número de variables escogidas
-			for (int j = 0; j < sol.getSelectedVars().length; j++) {
-				if (sol.getSelectedVars()[j])
-					numVars++;
-			}
-
-			// Cogemos los años anteriores al que vamos a testear
-			trainData = new double[4][numVars];
-			year = añoInicio;
-			for (int j = 0; j < trainData.length; j++) {
-				trainData[j] = data.getYear(year, sol.getSelectedVars());
-				year++;
-			}
-
-			// Se prueba la posible solución
-			elm = new elm(0, 20, "sig");
-			elm.train(trainData);
-
-			if (elm.getTrainingAccuracy() < bestAccuracy) { // Si es mejor se guarda
-				bestAccuracy = elm.getTrainingAccuracy();
-				bestSol = sol;
-				for (int j = 0; j < mostUsedVars.length; j++) {
-					if (sol.getSelectedVars()[j])
-						mostUsedVars[j]++;
+					if (elm.getTrainingAccuracy() < bestAccuracy) { // Si es mejor se guarda
+						bestAccuracy = elm.getTrainingAccuracy();
+						bestSol = sol;
+						for (int j = 0; j < mostUsedVars.length; j++) {
+							if (sol.getSelectedVars()[j])
+								mostUsedVars[j]++;
+						}
+					}
 				}
+
+				// Probamos antes cuando daría sin utilizar la busqueda local
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				trainData = new double[añosTrain][numVars];
+				year = añoInicio;
+				for (int i = 0; i < trainData.length; i++) {
+					trainData[i] = data.getYear(year, bestSol.getSelectedVars());
+					year++;
+				}
+
+				testData = new double[2][];
+				testData[0] = data.getYear(year, bestSol.getSelectedVars());
+				testData[1] = data.getYear(year, bestSol.getSelectedVars());
+				System.out.println("SIN MEJORA");
+
+				elm = new elm(0, 20, "sig");
+				elm.train(trainData);
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				output = elm.testOut(testData);
+
+				System.out.println(Arrays.toString(output));
+				
+				sb.append(output[0]);
+				sb.append(";");
+
+				// Busqueda local y resultados
+				exception = true;
+				while (exception) {
+					exception = false;
+					try {
+						bestSol = localSearch.improve(bestSol, mostUsedVars);
+					} catch (Exception e) {
+						exception = true;
+					}
+				}
+
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				trainData = new double[añosTrain][numVars];
+				year = añoInicio;
+				for (int i = 0; i < trainData.length; i++) {
+					trainData[i] = data.getYear(year, bestSol.getSelectedVars());
+					year++;
+				}
+
+				testData = new double[2][];
+				testData[0] = data.getYear(year, bestSol.getSelectedVars());
+				testData[1] = data.getYear(year, bestSol.getSelectedVars());
+				System.out.println("CON MEJORA");
+
+				elm = new elm(0, 20, "sig");
+				elm.train(trainData);
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				output = elm.testOut(testData);
+
+				System.out.println(Arrays.toString(output));
+				
+				sb.append(output[0]);
+				sb.append(";");
+
+				////////////////////////////////////////////////////////////////////////////
+
+				System.out.println("\n-----Votos Constructive-----");
+
+				constructive = new CVotos(nIterations);
+
+				bestSol = null;
+				bestAccuracy = Double.MAX_VALUE;
+
+				for (int i = 0; i < nIterations; i++) {
+					sol = constructive.generateSolution();
+					numVars = 0;
+
+					// Contamos el número de variables escogidas
+					for (int j = 0; j < sol.getSelectedVars().length; j++) {
+						if (sol.getSelectedVars()[j])
+							numVars++;
+					}
+
+					// Cogemos los años anteriores al que vamos a testear
+					trainData = new double[4][numVars];
+					year = añoInicio;
+					for (int j = 0; j < trainData.length; j++) {
+						trainData[j] = data.getYear(year, sol.getSelectedVars());
+						year++;
+					}
+
+					// Se prueba la posible solución
+					elm = new elm(0, 20, "sig");
+					elm.train(trainData);
+
+					if (elm.getTrainingAccuracy() < bestAccuracy) { // Si es mejor se guarda
+						bestAccuracy = elm.getTrainingAccuracy();
+						bestSol = sol;
+						for (int j = 0; j < mostUsedVars.length; j++) {
+							if (sol.getSelectedVars()[j])
+								mostUsedVars[j]++;
+						}
+					}
+				}
+
+				// Probamos antes cuando daría sin utilizar la busqueda local
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				trainData = new double[añosTrain][numVars];
+				year = añoInicio;
+				for (int i = 0; i < trainData.length; i++) {
+					trainData[i] = data.getYear(year, bestSol.getSelectedVars());
+					year++;
+				}
+
+				testData = new double[2][];
+				testData[0] = data.getYear(year, bestSol.getSelectedVars());
+				testData[1] = data.getYear(year, bestSol.getSelectedVars());
+				System.out.println("SIN MEJORA");
+
+				elm = new elm(0, 20, "sig");
+				elm.train(trainData);
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				output = elm.testOut(testData);
+
+				System.out.println(Arrays.toString(output));
+				
+				sb.append(output[0]);
+				sb.append(";");
+
+				// Busqueda local y resultados
+				exception = true;
+				while (exception) {
+					exception = false;
+					try {
+						bestSol = localSearch.improve(bestSol, mostUsedVars);
+					} catch (Exception e) {
+						exception = true;
+					}
+				}
+
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+				trainData = new double[añosTrain][numVars];
+				year = añoInicio;
+				for (int i = 0; i < trainData.length; i++) {
+					trainData[i] = data.getYear(year, bestSol.getSelectedVars());
+					year++;
+				}
+
+				testData = new double[2][];
+				testData[0] = data.getYear(year, bestSol.getSelectedVars());
+				testData[1] = data.getYear(year, bestSol.getSelectedVars());
+				System.out.println("CON MEJORA");
+
+				elm = new elm(0, 20, "sig");
+				elm.train(trainData);
+				numVars = 0;
+				for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
+					if (bestSol.getSelectedVars()[i])
+						numVars++;
+				}
+
+				output = elm.testOut(testData);
+
+				System.out.println(Arrays.toString(output));
+				
+				sb.append(output[0]);
+				sb.append("\n");
 			}
+			pw.write(sb.toString());
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-
-		bestSol = localSearch.improve(bestSol, mostUsedVars);
-
-		numVars = 0;
-		for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
-			if (bestSol.getSelectedVars()[i])
-				numVars++;
-		}
-		trainData = new double[añosTrain][numVars];
-		year = añoInicio;
-		for (int i = 0; i < trainData.length; i++) {
-			trainData[i] = data.getYear(year, bestSol.getSelectedVars());
-			year++;
-		}
-
-		testData = new double[2][];
-		testData[0] = data.getYear(year, bestSol.getSelectedVars());
-		testData[1] = data.getYear(year, bestSol.getSelectedVars());
-		System.out.println("Año buscado: " + añoBuscado);
-
-		elm = new elm(0, 20, "sig");
-		elm.train(trainData);
-		numVars = 0;
-		for (int i = 0; i < bestSol.getSelectedVars().length; i++) {
-			if (bestSol.getSelectedVars()[i])
-				numVars++;
-		}
-		
-		output = elm.testOut(testData);
-		
-		System.out.println(Arrays.toString(output));
+		/* CSV */
 	}
+
 }
